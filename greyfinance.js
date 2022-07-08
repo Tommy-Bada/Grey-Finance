fe49935309a67a217ad04b3f51af5867c41b675c
Bada tomiwa <tomiwabd@gmail.com>
2022-07-08 10:40:42 -0400

    Hover Effects

---
+++ greyfinance.js
@@ -0,0 +1,9 @@
+// Hamburger click
+// let hamburgerIcon = document.getElementById("hamburger-menu");
+// function openMobileMenu() {
+//     let mobileMenu = document.getElementById("hamburger-open");
+//     mobileMenu.style.opacity = "1";
+// }
+// hamburgerIcon.addEventListener("click", openMobileMenu);
+// console.log(document);
+alert("what the fuck");
