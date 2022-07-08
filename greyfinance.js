bbb5732b79c2949ffd2b6c015f251f268f052a21
Bada tomiwa <tomiwabd@gmail.com>
2022-07-08 12:41:06 -0400

    Mobile Menu Button

--- greyfinance.js
+++ greyfinance.js
@@ -1,9 +1,18 @@
-// Hamburger click
-// let hamburgerIcon = document.getElementById("hamburger-menu");
-// function openMobileMenu() {
-//     let mobileMenu = document.getElementById("hamburger-open");
-//     mobileMenu.style.opacity = "1";
-// }
-// hamburgerIcon.addEventListener("click", openMobileMenu);
-// console.log(document);
-alert("what the fuck");
+// Open Menu Bar
+let hamburgerIcon = document.getElementById("hamburger-menu");
+function openMobileMenu() {
+    let mobileMenu = document.getElementById("hamburger-open");
+    mobileMenu.style.opacity = 1;
+    mobileMenu.style.zIndex = 2;
+}
+hamburgerIcon.addEventListener("click", openMobileMenu);
+
+
+//Close Menu Bar
+let closeIcon = document.getElementById("close-btn");
+function closeMobileMenu(){
+    let mobileMenu = document.getElementById("hamburger-open");
+    mobileMenu.style.opacity = 0;
+    mobileMenu.style.zIndex = 0;
+}
+closeIcon.addEventListener("click", closeMobileMenu);
