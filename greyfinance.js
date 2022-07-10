049cdf43775c83d733e92157274dbeb95690d266
Bada tomiwa <tomiwabd@gmail.com>
2022-07-10 03:24:06 -0400

    indexing and dom manipulation

--- greyfinance.js
+++ greyfinance.js
@@ -58,7 +58,16 @@ function switchToKenya(){
     document.getElementById("section-4").style.display = "none";
     document.getElementById("section-5").style.display = "none";
     document.getElementById("Kenya-section").style.display = "block";
-
+    document.getElementById("section-2").firstElementChild.innerHTML = "Get a global account for all your international transactions";
+    document.getElementsByClassName("section-2-box-text1")[0].innerHTML = "<b>Get free foreign accounts and get paid by foreign employers & clients</b>"
+    document.getElementsByClassName("section-2-box-text2")[0].innerHTML = "Freelancer or remote worker? We’ve got you. Receive USD, GBP and EUR with ease in Kenya with your free international bank account."
+    document.getElementsByClassName("section-2-box-button")[0].innerHTML = "<b>Learn more →</b>"
+    document.getElementsByClassName("section-2-box-text1")[1].innerHTML = "<b>Convert foreign currencies instantly</b>"
+    document.getElementsByClassName("section-2-box-text2")[1].innerHTML = "Quickly convert your foreign earnings to KES on Grey at competitive market rates. Get your money’s full value every time."
+    document.getElementsByClassName("section-2-box-button")[1].innerHTML = "<b>Learn more →</b>"
+    document.getElementsByClassName("section-2-box-text1")[2].innerHTML = "<b>Transfer Money</b>"
+    document.getElementsByClassName("section-2-box-text2")[2].innerHTML = "Send money to any foreign account directly from your Grey account. No stress. No delay. No hidden fees. It's that easy."
+    document.getElementsByClassName("section-2-box-button")[2].innerHTML = "<b>Learn more →</b>"
 
 }
 function switchToNigeria(){
@@ -73,7 +82,16 @@ function switchToNigeria(){
     document.getElementById("section-4").style.display = "block";
     document.getElementById("section-5").style.display = "block";
     document.getElementById("Kenya-section").style.display = "none";
-
+    document.getElementById("section-2").firstElementChild.innerHTML = "Free USD, GBP and EUR accounts for all your international transactions"
+    document.getElementsByClassName("section-2-box-text1")[0].innerHTML = "<b>Get paid by foreign employers and clients</b>"
+    document.getElementsByClassName("section-2-box-text2")[0].innerHTML = "Use your account details to receive pounds, dollars and euros like a local and manage your earnings. Invoice like a local and manage your earnings."
+    document.getElementsByClassName("section-2-box-button")[0].innerHTML = "<b>Open a virtual bank account</b>"
+    document.getElementsByClassName("section-2-box-text1")[1].innerHTML = "<b>Instant currency exchange</b>"
+    document.getElementsByClassName("section-2-box-text2")[1].innerHTML = "Enjoy quick and efficient online foreign exchange transactions. Say goodbye to bank queues and disappointments."
+    document.getElementsByClassName("section-2-box-button")[1].innerHTML = "<b>Convert your foreign currency</b>"
+    document.getElementsByClassName("section-2-box-text1")[2].innerHTML = "<b>Send and receive money quickly</b>"
+    document.getElementsByClassName("section-2-box-text2")[2].innerHTML = "Enjoy fast global money transfer with your international bank account on Grey."
+    document.getElementsByClassName("section-2-box-button")[2].innerHTML = "<b>Send Money Abroad</b>"
 }
 kenyaBtn.addEventListener("click", switchToKenya);
 nigeriaBtn.addEventListener("click", switchToNigeria);
